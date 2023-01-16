db6bf8b449395dcddcb1959fe1e900a83515898c
DeborahOk <deborah.okunlola@hotmail.com>
2023-01-16 13:25:15 +0000

    initial version

---
+++ src/BankSavings.java
@@ -0,0 +1,10 @@
+public class BankSavings {
+    public static void main(String[] args) {
+        int savingsMoney = 86000;
+        if (savingsMoney >= 0 && savingsMoney <= 85000) {
+            System.out.println("You are protected");
+        } else {
+            System.out.println("You are not protected");
+        }
+    }
+}
